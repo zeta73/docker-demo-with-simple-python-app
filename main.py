139222327c3cf3058d07335ce2ab90259c6b4676
Josifha Ashmi J <65861420+Josifha@users.noreply.github.com>
2022-12-05 19:28:03 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'welcome to alibaba cloud'
+    return 'welcome to alibaba cloud v2'
 
 @app.errorhandler(500)
 def server_error(e):
