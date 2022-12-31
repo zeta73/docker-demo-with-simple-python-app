93fb975731b50658e94767cf2bbd72c3229dea3f
Josifha Ashmi J <65861420+Josifha@users.noreply.github.com>
2022-12-31 18:20:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     """Return a friendly HTTP greeting."""
-    return 'Welcome   to Cloud'
+    return 'Welcome no to Cloud'
 
 @app.errorhandler(500)
 def server_error(e):
