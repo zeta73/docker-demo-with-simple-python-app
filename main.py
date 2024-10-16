d1909a0fe2eff88a2fba107b1cb147fe6d64d52e
zeta73 <53174991+zeta73@users.noreply.github.com>
2024-10-16 16:21:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,9 @@
-import logging
-
-from flask import Flask, request, jsonify
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    """Return a friendly HTTP greeting."""
-    return 'Welcome to cloud run'
+def index():
+  return '<h1><center>This Sample Flask Application</center></h1>'
 
-@app.errorhandler(500)
-def server_error(e):
-    logging.exception('An error occurred during a request.')
-    return """
-    An internal error occurred: <pre>{}</pre>
-    See logs for full stacktrace.
-    """.format(e), 500
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+app.run(host='0.0.0.0', port=5000)
