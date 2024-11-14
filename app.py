0125648b67fc089c62ee4dfe72beb728f7d09e9f
zeta73 <rydityaputra@gmail.com>
2024-11-14 16:16:11 +0700

    ad v.3

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1><center>new version apps v.2.0</center></h1>'
+  return '<h1><center>hello, thank you for update to new version apps v.3.0</center></h1>'
 
 app.run(host='0.0.0.0', port=5000)
