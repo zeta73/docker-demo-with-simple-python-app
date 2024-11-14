f21d02357e0d7087ff4b5126b4c75b1a9595a351
zeta73 <rydityaputra@gmail.com>
2024-11-14 11:21:44 +0700

    edit title

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return '<h1><center>Hello, currently flask running on k8s</center></h1>'
+  return '<h1><center>new version apps v.1.1</center></h1>'
 
 app.run(host='0.0.0.0', port=5000)
